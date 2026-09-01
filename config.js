c836eba2f2be77afb8a516a1d003a157484bfd4b
CodexSandboxOffline <CodexSandboxOffline@CNT-ANHVN1.sav.gov.vn>
2026-09-01 16:52:02 +0700

    Fix admin schedule filter and expand quiz questions

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DASHBOARD_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbyTomCQ_7QtDH89H4LrXDphpRKFQUpnqR-0kuqHw8-NoibLp1TKsbuD-xTBmZrJiX3d/exec",
-  adminUrl: "https://script.google.com/macros/s/AKfycbzfUYvxmf2jRAgK-luAfjX28V0tc7gcpP25QKSlzc8nJJuVW_4d4yx0Ha8CSTwg-jlYUA/exec",
+  adminUrl: "https://script.google.com/macros/s/AKfycbzwc2eAjIIpU-io_Skty8sxnNmQLe4w7ZC2Jo8f_efQZbA1gdcrbTubnaDnyA4YL2W4Ug/exec",
   refreshSeconds: 10,
   demoDataUrl: "data/demo.json",
   fakeDataUrl: "data/fake.json"
