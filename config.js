fb29dbb396c60debc09c96c7845d3c964beea8e9
CodexSandboxOffline <CodexSandboxOffline@CNT-ANHVN1.sav.gov.vn>
2026-09-01 16:25:41 +0700

    Group admin sessions by teaching schedule

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DASHBOARD_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbyTomCQ_7QtDH89H4LrXDphpRKFQUpnqR-0kuqHw8-NoibLp1TKsbuD-xTBmZrJiX3d/exec",
-  adminUrl: "https://script.google.com/macros/s/AKfycbwQpQnLN6GsnLuiLSdJv6lCi_X47Sz2rnFuerg9il--YIc6vV-jLyAyCUAxSGN_JszsMg/exec",
+  adminUrl: "https://script.google.com/macros/s/AKfycbzfUYvxmf2jRAgK-luAfjX28V0tc7gcpP25QKSlzc8nJJuVW_4d4yx0Ha8CSTwg-jlYUA/exec",
   refreshSeconds: 10,
   demoDataUrl: "data/demo.json",
   fakeDataUrl: "data/fake.json"
